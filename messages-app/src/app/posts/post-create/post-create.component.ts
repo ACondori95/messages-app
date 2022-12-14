260047b79d3444fc64ef5a19624f6202815dc039
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-14 13:50:31 -0300

    call GET post

--- messages-app/src/app/posts/post-create/post-create.component.ts
+++ messages-app/src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -10,9 +10,8 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
 
-  constructor() {}
+  constructor(public postsService: PostsService) {}
 
   ngOnInit(): void {}
 
@@ -20,10 +19,6 @@ export class PostCreateComponent implements OnInit {
     if (form.invalid) {
       return;
     }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content,
-    };
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
