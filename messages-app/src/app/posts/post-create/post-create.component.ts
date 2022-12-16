f4d68508d4264b3afdfc8b168bcb14c533c16ccf
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-16 17:49:29 -0300

    create the edit form

--- messages-app/src/app/posts/post-create/post-create.component.ts
+++ messages-app/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
 @Component({
@@ -10,10 +12,27 @@ import { PostsService } from '../posts.service';
 export class PostCreateComponent implements OnInit {
   enteredContent = '';
   enteredTitle = '';
+  private mode = 'create';
+  private postId: string;
+  private post: Post;
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
