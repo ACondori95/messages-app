54c5141c39d8bd2ee72a1238a5773a0cc0342369
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-15 23:20:59 -0300

    fetch data from database

--- messages-app/backend/app.js
+++ messages-app/backend/app.js
@@ -41,21 +41,11 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "123456789",
-      title: "First server-side post",
-      content: "This is comming from the browser",
-    },
-    {
-      id: "987654321",
-      title: "Second server-side post",
-      content: "This is comming from the browser!",
-    },
-  ];
-  res
-    .status(200)
-    .json({ message: "Posts fetched successfully!", posts: posts });
+  Post.find().then((documents) => {
+    res
+      .status(200)
+      .json({ message: "Posts fetched successfully!", posts: documents });
+  });
 });
 
 module.exports = app;
