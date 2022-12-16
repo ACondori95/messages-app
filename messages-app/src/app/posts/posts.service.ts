7eb4a251f807f0d4f34b9d8d1d389651e32aa52d
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-15 23:39:32 -0300

    transform response data

--- messages-app/src/app/posts/posts.service.ts
+++ messages-app/src/app/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { map, Subject } from 'rxjs';
 import { Post } from './post.model';
 
 @Injectable({
@@ -14,11 +14,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
       )
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -28,7 +37,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { id: null, title: title, content: content };
+    const post: Post = { _id: null, title: title, content: content };
     this.http
       .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
