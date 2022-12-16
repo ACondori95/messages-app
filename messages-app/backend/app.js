5f461b3ad3ac62b032bd2ca0aa5ea6c152426176
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-16 13:14:24 -0300

    add posts with an id

--- messages-app/backend/app.js
+++ messages-app/backend/app.js
@@ -36,8 +36,11 @@ app.use((req, res, next) => {
 
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({ title: req.body.title, content: req.body.content });
-  post.save();
-  res.status(201).json({ message: "Post added successfully" });
+  post.save().then((createdPost) => {
+    res
+      .status(201)
+      .json({ message: "Post added successfully", postId: createdPost._id });
+  });
 });
 
 app.get("/api/posts", (req, res, next) => {
