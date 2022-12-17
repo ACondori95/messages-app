530d1b975135f0a8f039443e43e964f52b4a6e95
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-17 14:48:32 -0300

    component basics

--- basic-assignment-1/src/app/app.module.ts
+++ basic-assignment-1/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
