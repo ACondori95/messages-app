33f159786bef62fcd11a90e8fbcbc73ea3901615
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-19 20:20:32 -0300

    output a list of recipes with ngFor

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,12 @@ export class RecipeListComponent {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw1i2GTikcXV2657GIYwS6kS&ust=1671556014010000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMiju8WVhvwCFQAAAAAdAAAAABAE'
+      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'
     ),
   ];
 }
