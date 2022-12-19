ae9b689213de2fd6c9ff89b26ca3a9dbcc48f308
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-19 20:08:40 -0300

    add content to the recipe components

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw1i2GTikcXV2657GIYwS6kS&ust=1671556014010000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMiju8WVhvwCFQAAAAAdAAAAABAE'
+    ),
+  ];
 }
