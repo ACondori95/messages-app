818c4d1ce8a7291c9d6e92560ac771da556d25da
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-12-24 14:37:53 -0300

    inject services into services

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
