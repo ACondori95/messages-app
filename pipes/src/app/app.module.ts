f72b4bc1ea3c467879ed4d1a509bfee1b0ffba24
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2023-01-06 21:20:30 -0300

    create a custom pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShortenPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
