a94b477c16b9cd946a474ff7926070521adb3503
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2023-01-12 23:36:20 -0300

    send a delete request

--- http-01/src/app/posts.service.ts
+++ http-01/src/app/posts.service.ts
@@ -38,4 +38,10 @@ export class PostsService {
         })
       );
   }
+
+  deletePosts() {
+    return this.http.delete(
+      'https://ng-complete-guide-dd596-default-rtdb.firebaseio.com/posts.json'
+    );
+  }
 }
